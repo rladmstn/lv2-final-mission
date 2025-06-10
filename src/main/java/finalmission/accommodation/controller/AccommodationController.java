9b02ec98158845a1d9e0a78d169aa8d666dfa59b
rladmstn <oz33177351@gmail.com>
2025-06-10 14:25:56 +0900

    fix: request body 오류 수정

--- src/main/java/finalmission/accommodation/controller/AccommodationController.java
+++ src/main/java/finalmission/accommodation/controller/AccommodationController.java
@@ -6,6 +6,7 @@ import finalmission.accommodation.service.AccommodationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class AccommodationController {
     }
 
     @PostMapping
-    public ResponseEntity<AccommodationResponse> create(CreateAccommodationRequest request) {
+    public ResponseEntity<AccommodationResponse> create(@RequestBody CreateAccommodationRequest request) {
         AccommodationResponse response = accommodationService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
