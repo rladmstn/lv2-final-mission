b297436a1e93c2590c1e05d2df31e852afe2811a
rladmstn <oz33177351@gmail.com>
2025-06-17 22:49:41 +0900

    test: 테스트 환경 세팅 추가

--- src/test/java/finalmission/auth/controller/AuthControllerTest.java
+++ src/test/java/finalmission/auth/controller/AuthControllerTest.java
@@ -13,9 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@Sql(scripts = "/clean.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 class AuthControllerTest {
 
     @LocalServerPort
